17980cc2b300123b37d14007fe4fb7c823dbbcdd
cocain <needamface@yahoo.com>
2013-06-23 20:32:46 +0400

    edit

--- yandexonair.php
+++ yandexonair.php
@@ -1,7 +1,7 @@
 <?php
 
 $n = 1;
-$h = fopen('./resuuniq', 'w');
+$h = fopen('./result', 'w');
 while(true) {
 	print "[+] query: {$n}\n";
 	$curl = `curl -b -e 'Referer: http://swf.static.yandex.net/queries/live-broadcast.swf?v=10.34.5&data-url=http://livequeries-front.corba.yandex.net/queries/' --user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0" 'http://livequeries-front.corba.yandex.net/queries/?ll1=-1.7356459922329044,-9.520975999999978&ll2=84.54750547846963,216.88527400000004&limit=50000&nc=0.027750199660658836' 2>&1`;
